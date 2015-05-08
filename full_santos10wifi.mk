acb4bae9843a2671a344c2d9906643895b3a68e7
Ziyan <jaraidaniel@gmail.com>
2015-05-08 02:47:02 +0200

    Use 44.1 kHz UI sounds
    Our fast paths sampling rate is 44.1 kHz, which
    conflicts with the default 48 kHz sounds.
    
    Change-Id: I1505b88180b5db6efb70ad6882c3b12a10989782

--- full_santos10wifi.mk
+++ full_santos10wifi.mk
@@ -17,6 +17,9 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
+# Use 44.1 kHz UI sounds
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage13.mk)
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
